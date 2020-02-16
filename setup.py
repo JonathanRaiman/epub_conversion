85a66deaabe8bc72acf5cae11c18190be22aed95
Jonathan Raiman <jonathanraiman@gmail.com>
2020-02-15 23:56:30 -0800

    pep8 + fix broken arg

--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
 import os
 from setuptools import setup, find_packages
 
+
 def readfile(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
 setup(
     name='epub-conversion',
-    version='1.0.7',
+    version='1.0.8',
     description='Python package for converting xml and epubs to text files',
     long_description=readfile('README.md'),
     ext_modules=[],
     packages=find_packages(),
-    py_modules = [],
+    py_modules=[],
     author='Jonathan Raiman',
     author_email='jraiman at mit dot edu',
     url='https://github.com/JonathanRaiman/epub_conversion',
@@ -26,12 +28,11 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Topic :: Text Processing :: Linguistic',
     ],
-    # test_suite="something.test",
-    setup_requires = [],
+    setup_requires=[],
     install_requires=[
         'bz2file',
         'epub',
-        'xml_cleaner'
+        'ciseau'
     ],
     include_package_data=True,
 )
