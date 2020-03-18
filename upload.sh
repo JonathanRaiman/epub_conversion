d4522997e7281aea05ba357dbed0c92d7dc7ba61
Jonathan Raiman <jonathanraiman@gmail.com>
2020-03-18 12:13:34 +0100

    update version + add old file removeré

--- upload.sh
+++ upload.sh
@@ -7,5 +7,9 @@ set -u
 # handle cascading failures well
 set -o pipefail
 
+SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+rm ${SCRIPT_DIR}/dist/*
+python3 setup.py clean
 python3 setup.py sdist
-twine upload dist/*
+twine upload ${SCRIPT_DIR}/dist/* --verbose
